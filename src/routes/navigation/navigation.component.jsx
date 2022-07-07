c91d09dee0f2470aa47078f8cea78d5c4e68aa25
TraceyTaylor <107629102+TraceyTaylor@users.noreply.github.com>
2022-07-07 16:25:08 -0500

    Section 7: React Context completed

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,7 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import "./navigation.styles.scss";
 
@@ -19,13 +19,15 @@ const Navigation = () => {
         <Link className="logo-container" to="/">
           <CrwnLogo className="logo" />
         </Link>
-        <div className="links-container">
+        <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
+
           {currentUser ? (
-            <span className="nav-link" onClick={signOutUser}>
-              SIGN OUT
+            <span className="nav-link" onClick={signOutHandler}>
+              {" "}
+              SIGN OUT{" "}
             </span>
           ) : (
             <Link className="nav-link" to="/auth">
